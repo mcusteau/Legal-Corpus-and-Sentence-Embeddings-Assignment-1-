7ce36d72b30ce936ed10524d290d763610e5a403
Michel Custeau <custeaumichel@gmail.com>
2022-09-23 19:57:02 -0400

    added regex rules to tokenizer

--- corpus_processing.py
+++ corpus_processing.py
@@ -28,7 +28,6 @@ def tokenise(doc):
 	find_abbreviations = r'''(?<=[\s\t])[A-Z][\.a-zA-Z]*\.(?=[\s\t])''' # finds abbreviations like "St." and "P.H.D"
 
 	find_abbreviations2 = r'''(?<=[\s\t])[a-z\d]+\.[a-z\.\d]+(?=[\s\t])''' #finds abbreviations like i.e. or 10.19
-	test =r'''https:[^\s\t]+'''
 
 	abbreviations_dict = {}
 
