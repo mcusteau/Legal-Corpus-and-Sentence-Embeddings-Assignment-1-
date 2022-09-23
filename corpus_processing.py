81dbcf1d0177508868699c374fd5c2a235c5ef2e
Michel Custeau <custeaumichel@gmail.com>
2022-09-23 19:55:57 -0400

    added regex rules to tokenizer

--- corpus_processing.py
+++ corpus_processing.py
@@ -1,16 +1,62 @@
 import os
+import spacy
+from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+from spacy.symbols import ORTH
+import re
 
 files = ""
 
+nlp = English()
+
 def mergeFiles(directory="./CUAD_v1/full_contract_txt/"):
 
-	files = ""
+	files = " "
 	for filename in os.listdir(directory):
 		with open(os.path.join(directory,  filename)) as f:
-			files += " " + f.read()
+			files += f.read() + " "
 	return files		
 
 
 file_string = mergeFiles()
 
-print(file_string)
+# create tokens
+def tokenise(doc):
+	
+	#reg = '''[\.'](?=[\s\t])|[\[\("\]\)\:\?\!\:]|(?<=[\s\t])[']'''
+
+	find_abbreviations = r'''(?<=[\s\t])[A-Z][\.a-zA-Z]*\.(?=[\s\t])''' # finds abbreviations like "St." and "P.H.D"
+
+	find_abbreviations2 = r'''(?<=[\s\t])[a-z\d]+\.[a-z\.\d]+(?=[\s\t])''' #finds abbreviations like i.e. or 10.19
+	test =r'''https:[^\s\t]+'''
+
+	abbreviations_dict = {}
+
+	abbreviations = re.findall(find_abbreviations, doc)
+	print(abbreviations)
+	for abb in abbreviations:
+		abbreviations_dict[abb] = [{ORTH: abb}]
+
+	abbreviations2 = re.findall(find_abbreviations2, doc)
+	print(abbreviations2)
+	for abb in abbreviations2:
+		abbreviations_dict[abb] = [{ORTH: abb}]
+
+	prefix_regex = re.compile(r'''^[\[\("']''')
+
+	suffix_regex = re.compile(r'''[\]\)\:\?\!\:\."']''')
+
+	http_catcher = re.compile(r'''https:[^\s\t]+''')
+
+	tokenizer = Tokenizer(nlp.vocab, rules = abbreviations_dict, prefix_search = prefix_regex.search, suffix_search=suffix_regex.search, url_match=http_catcher.search)
+
+	doc = tokenizer(doc)
+
+	tokens = [token.text for token in doc]
+	
+	return tokens
+
+
+tokens = tokenise(file_string)
+
+#print(tokens)
